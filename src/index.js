94c5ccef44ea02860f402407156099240cc45d4e
yearninGG <73342236+yearninGG@users.noreply.github.com>
2021-07-04 12:35:48 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
+'use strict'
+
 module.exports = function reverse (n) {
-  
+    if (n < 0) {
+        n = -n
+    }
+
+    n = String(n)
+    let reverseNumber = ''
+
+    for (let i = 1; i <= n.length; i++) {
+        reverseNumber += (n[n.length - i])
+    }
+    return reverseNumber
 }
